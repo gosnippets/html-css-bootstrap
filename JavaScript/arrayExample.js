b77b65b5bfe1c21080e6eca17b45770477ecf75f
Ramdas Gupta <er.rdgupta@gmail.com>
2023-01-27 19:55:12 +0530

    Js Array

---
+++ JavaScript/arrayExample.js
@@ -0,0 +1,54 @@
+// var num1 = 1;
+// var num2 = "2";
+// var num3 = 3;
+
+// var arry = [num1, num2, num3];
+// console.log(arry);
+
+// console.log("Length", arry.length)
+
+// console.log(arry[1])
+
+// const arry1 = [1, 2, 3, 4, 5];
+// const arry2 = ['a', 'b', 'c', ...arry1];
+
+// arry1[5] = 6;
+// arry2[3] = 'd';
+
+// const arry3 = [...arry1, ...arry2]
+
+// console.log(arry1)
+// console.log(arry2)
+// console.log(arry3)
+
+// const arry4 = new Array('a',2,3,4);
+// console.log(arry4);
+
+const names = ["Ram", "Shyam", "Ravi", "Surya"];
+// document.getElementById("str").innerHTML =names.toString()
+// console.log(names.toString());
+
+// names.push("Maya");
+// names.pop();
+// names.pop();
+// names.pop();
+
+// console.log(names.shift())
+// console.log(names.unshift("Ravi"));
+// console.log(names.unshift("Surya"));
+
+// delete names[1];
+// delete names[2];
+// delete names[3];
+
+// Splice
+// Slice
+
+// names.splice(1, 0, "Maya", "Mukesh");
+// names.splice(0,1);
+
+console.log(names.slice(1));
+console.log(names.slice(0, 1));
+
+
+console.log(names);
