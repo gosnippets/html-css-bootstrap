83f694d0e2334e121197a3343320d52a212607d2
Ramdas Gupta <er.rdgupta@gmail.com>
2023-01-28 09:39:43 +0530

    array

--- JavaScript/arrayExample.js
+++ JavaScript/arrayExample.js
@@ -24,7 +24,7 @@
 // const arry4 = new Array('a',2,3,4);
 // console.log(arry4);
 
-const names = ["Ram", "Shyam", "Ravi", "Surya"];
+// const names = ["Ram", "Shyam", "Ravi", "Surya"];
 // document.getElementById("str").innerHTML =names.toString()
 // console.log(names.toString());
 
@@ -47,8 +47,52 @@ const names = ["Ram", "Shyam", "Ravi", "Surya"];
 // names.splice(1, 0, "Maya", "Mukesh");
 // names.splice(0,1);
 
-console.log(names.slice(1));
-console.log(names.slice(0, 1));
+// console.log(names.slice(1));
+// console.log(names.slice(0, 1));
 
+// console.log(names);
 
-console.log(names);
+var arry5 = ["a", "b", "c"];
+var arry6 = ["d", "e", "f"];
+
+// var arry7 = [...arry5, ...arry6];
+// arry5.push(...arry6);
+// console.log(arry7)
+// console.log(arry5)
+// arry5.splice(1, 0, ...arry6);
+// console.log(arry5);
+
+//  var arry8 = arry5.concat(arry6);
+//  console.log(arry5, arry8)
+
+// console.log(arry5.toString());
+// console.log(arry5.join("*"));
+
+// Sorting
+// const num1 = [10, 2, 60, 5, 7];
+// num1.sort((n1, n2) => n1 - n2);
+// console.log(num1);
+// console.log("Smallest=", num1[0], "Greatest=", num1[num1.length-1]);
+
+// num1.sort((n1, n2) => n2 - n1);
+// console.log(num1);
+// console.log("Smallest=", num1[num1.length-1], "Greatest=", num1[0]);
+
+const text1 = ["z", "a", "f", "b", "c"];
+// console.log(text1.sort());
+// console.log(text1[0],text1[text1.length-1])
+// console.log(text1.reverse());
+// console.log(text1[text1.length-1],text1[0])
+
+// text1.map((value, index, arry)=>{
+//     console.log(index, value, arry);
+// })
+
+text1.map(myFunc);
+
+function myFunc(value, index, arry) {
+  console.log(index, value, arry);
+}
+
+text1.forEach((value, index, arry)=> console.log(index, value, arry))
+text1.forEach(myFunc);
