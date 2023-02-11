e63f917333089d391e201f0ab72ea5a110c773f3
Ramdas Gupta <er.rdgupta@gmail.com>
2023-02-11 09:19:03 +0530

    guess your number

--- JavaScript/dom.js
+++ JavaScript/dom.js
@@ -24,42 +24,61 @@ generateRandomNumber();
 
 function generateRandomNumber() {
     randomNumber = Math.trunc(Math.random() * 20) + 1;
-    document.querySelector(".random-number").textContent = randomNumber
 }
 
 let statusEl = document.querySelector(".status");
+let checkBtnEl = document.querySelector(".check-btn");
+let bodyEl = document.querySelector("body");
+let inputNumberEl = document.querySelector(".input-number");
+let remainingChanceEl = document.querySelector(".remaining-chance")
+
 let remaningChance = 20;
 let bestScore = 0;
+let statusStr = "";
 
 function decrementChance() {
     remaningChance--;
-    document.querySelector(".remaining-chance").textContent = remaningChance;
+    remainingChanceEl.textContent = remaningChance;
 }
 
 function checkNumber() {
-    let inputNumber = Number(document.querySelector(".input-number").value);
-    decrementChance();
+    let inputNumber = Number(inputNumberEl.value);
+
     if (inputNumber === randomNumber) {
-        if(bestScore<=remaningChance) bestScore = remaningChance;
-        document.querySelector(".best-score").textContent = bestScore
-        statusEl.innerHTML = "<span class='text-success'>You Won!</span>"
-    } else if (inputNumber > 20 || inputNumber < 1) {
-        statusEl.innerHTML = "<span class='text-danger'>Invalid Input</span>"
-    } else if (inputNumber > randomNumber) {
-        statusEl.innerHTML = "<span class='text-warning'>Too High!</span>"
-    } else if (inputNumber < randomNumber) {
-        statusEl.innerHTML = "<span class='text-warning'>Too Low!</span>"
+        if (bestScore <= remaningChance) {
+            bestScore = remaningChance;
+            document.querySelector(".best-score").textContent = bestScore
+        }
+        statusStr = "<span class='text-success'>You Won!</span>";
+        checkBtnEl.disabled = true;
+        bodyEl.style.backgroundColor = "#71d8b8";
+        document.querySelector(".random-number").textContent = randomNumber;
+
+    } else {
+        if (remaningChance <= 1) {
+            checkBtnEl.disabled = true;
+            statusStr = "<span class='text-danger'>Game over!</span>";
+        } else {
+            statusStr = (inputNumber > 20 || inputNumber < 1) ? "<span class='text-danger'>Invalid Input</span>" :
+                (inputNumber > randomNumber) ? "<span class='text-warning'>Too High!</span>" :
+                    (inputNumber < randomNumber) ? "<span class='text-warning'>Too Low!</span>":null
+        }
     }
 
+    statusEl.innerHTML = statusStr;
+    decrementChance();
 }
 
 function resetGame() {
     generateRandomNumber();
     remaningChance = 20;
-    document.querySelector(".remaining-chance").textContent = remaningChance;
-    document.querySelector(".input-number").value=""
+    remainingChanceEl.textContent = remaningChance;
+    inputNumberEl.value = ""
+    checkBtnEl.disabled = false;
+    statusEl.innerHTML = "<span>Your status</span>";
+    bodyEl.style.backgroundColor = "white";
 }
 
-document.querySelector(".check-btn").addEventListener("click", () => checkNumber())
+checkBtnEl.addEventListener("click", () => checkNumber())
 document.querySelector(".play-again-btn").addEventListener("click", () => resetGame())
 
