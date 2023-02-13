e5811a6f3e9e78172948d787bb45888198c37bb7
Ramdas Gupta <er.rdgupta@gmail.com>
2023-02-13 09:35:07 +0530

    pig game\

---
+++ JavaScript/pig-game/main.js
@@ -0,0 +1,12 @@
+const score0El = document.getElementById("score--0");
+const score1El = document.querySelector("#score--1");
+const current0El = document.querySelector("#current--0");
+const current1El = document.querySelector("#current--1");
+
+const diceEl = document.querySelector(".dice");
+const btnNewEl = document.querySelector(".btn--new")
+const btnRollEl = document.querySelector(".btn--roll")
+const btnHoldEl = document.querySelector(".btn--hold")
+
+console.log("score0El", score0El)
+console.log("score1El", score1El)
