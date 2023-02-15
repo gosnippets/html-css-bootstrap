c38a346c9a8510987051e07945ffa1f7015e9bed
Ramdas Gupta <er.rdgupta@gmail.com>
2023-02-15 09:39:09 +0530

    dom

--- JavaScript/pig-game/main.js
+++ JavaScript/pig-game/main.js
@@ -10,22 +10,39 @@ const btnNewEl = document.querySelector(".btn--new")
 const btnRollEl = document.querySelector(".btn--roll")
 const btnHoldEl = document.querySelector(".btn--hold")
 
-let currentScore = 0, activePlayer = 0;
-let scores = [0, 0]
-current0El.textContent = 0
-current1El.textContent = 0
+let currentScore, activePlayer;
+let scores;
 
+const init = function () {
+    currentScore = 0;
+    activePlayer = 0;
+
+    scores = [90, 90]
+    score0El.textContent = scores[0];
+    score1El.textContent = scores[1];
+
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add("hidden");
+    btnRollEl.disabled = false;
+    btnHoldEl.disabled = false
+
+    player0El.classList.add("player--active");
+    player1El.classList.remove("player--active");
+
+    player0El.classList.remove("player--winner")
+    player1El.classList.remove("player--winner")
+}
+
+init();
 
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0
     activePlayer = activePlayer === 0 ? 1 : 0;
     currentScore = 0;
-    console.log("Active Player", activePlayer);
-
     player0El.classList.toggle("player--active");
     player1El.classList.toggle("player--active");
-
-
 }
 
 btnRollEl.addEventListener("click", function () {
@@ -38,11 +55,23 @@ btnRollEl.addEventListener("click", function () {
     } else {
         switchPlayer();
     }
-
-
-
-
-
-
-
 })
+
+btnHoldEl.addEventListener("click", function () {
+    scores[activePlayer] = scores[activePlayer] + currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
+    if(scores[activePlayer]>90 && scores[activePlayer]<100){
+        console.log("You are very close");
+    }
+
+    if (scores[activePlayer] >= 100) {
+        document.querySelector(`.player--${activePlayer}`).classList.add("player--winner")
+        document.querySelector(`.player--${activePlayer}`).classList.remove("player--active")
+        btnRollEl.disabled = true;
+        btnHoldEl.disabled = true
+    } else {
+        switchPlayer();
+    }
+})
+
+btnNewEl.addEventListener("click", init)
