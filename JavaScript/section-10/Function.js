b243c2bff7f651355ca6cc1c51eaa206f1950cd0
Ramdas Gupta <er.rdgupta@gmail.com>
2023-03-02 09:26:08 +0530

    Call, apply and bind methods

--- JavaScript/section-10/Function.js
+++ JavaScript/section-10/Function.js
@@ -116,60 +116,238 @@
 // console.log(doubledNum)
 
 // 2. Filter
-const numArray = [1, 2, 3, 4, 5];
+// const numArray = [1, 2, 3, 4, 5];
 
-function checkEven(value, index) {
-    console.log("index", index)
-    return value % 2 === 0;
+// function checkEven(value, index) {
+//     console.log("index", index)
+//     return value % 2 === 0;
+// }
+// const evenNum = numArray.filter(checkEven);
+// console.log(evenNum)
+
+// // 3. Custom HOF
+// function sayHello(index) {
+//     console.log(index, "Hello World!")
+// }
+
+// function repeatFunction(func, n) {
+//     for (let i = 0; i < n; i++) {
+//         func(i)
+//     }
+// }
+
+// repeatFunction(sayHello, 3)
+
+
+// const numArray2 = [1, 2, 3, 4, 5];
+// function callBackExample(numArray) {
+//     let newArray = [];
+//     for (let value of numArray) {
+//         if (value % 2 === 0) {
+//             newArray.push(value)
+//         }
+//     }
+//     return newArray
+// }
+// function getEvenNumber(fn) {
+//     return fn(numArray2);
+// }
+
+// const abc = getEvenNumber(callBackExample)
+// console.log(abc)
+
+// // =====
+// function show(num) {
+//     console.log("Result =", num)
+// }
+
+// function operation(a, b, callbackFn) {
+//     let sum = a + b;
+//     callbackFn(sum)
+// }
+
+// operation(10, 20, show)
+
+// // Function Accepting Callback Functions
+// function add(a, b) {
+//     console.log("Add function is called...")
+//     return a + b;
+// }
+
+// function sub(a, b) {
+//     console.log("Sub function is called...")
+//     return a - b;
+// }
+
+// function Calculate(num1, num2, operation) {
+//     return operation(num1, num2)
+// }
+
+// const result = Calculate(10, 5, sub)
+// console.log(result)
+
+// # Function Returning Functions
+
+// 1
+// const greet = function(greeting){
+//     console.log(greeting)
+//     return function(name){
+//         console.log(greeting,name)
+//     }
+// }
+// const greetHey = greet("Hey");
+// greetHey("Surya")
+
+// // 2
+// function multiplyBy(basenum) {
+//     return function(num){
+//         console.log(basenum * num)
+//         return basenum * num;
+//     }
+// }
+
+// const multiplyByTwo = multiplyBy(2)
+// multiplyByTwo(1);
+// multiplyByTwo(2);
+// multiplyByTwo(3);
+// multiplyByTwo(4);
+
+
+// # Call Method
+// 1
+// function greet() {
+//     console.log("Hello", this.name);
+// }
+// const person = { name: "Surya", age: 32 }
+// greet.call(person);
+
+// 2
+// const person2 = {
+//     name: 'Surya',
+//     greet: function () {
+//         console.log("Hello", this.name, this.age);
+//     }
+// }
+// person2.greet() // 
+// person2.greet.call({ name: 'Ravi', age: 32 })
+
+// 3
+// function Person(name, age) {
+//     this.name = name
+//     this.age = age
+//     this.greet = function () {
+//         console.log("Hello", this.name, this.age);
+//     }
+// }
+// const person3 = {}
+// Person.call(person3, 'Surya', 30);
+// person3.greet();
+
+// // 4
+// function Sum4(a, b, c) {
+//     console.log("Sum =", a + b + c)
+// }
+
+// Sum4.call(null, 2, 5, 3);
+
+// # Apply Method 
+// 1
+// function Sum4(a, b, c) {
+//     console.log("Sum =", a + b + c)
+// }
+
+// Sum4.apply(null, [2, 5, 6]); //
+
+// // 2
+// function greet(greeting) {
+//     console.log(greeting, this.name)
+// }
+
+// greet.apply({ name: 'Ravi' }, ['Hello'])
+
+// // 3
+// function Person(name, age) {
+//     this.name = name
+//     this.age = age
+//     this.greet = function () {
+//         console.log("Hello", this.name, this.age);
+//     }
+// }
+// const person3 = {}
+// Person.apply(person3, ['Surya', 30]);
+// person3.greet();
+
+// //
+// const obj = { firstName: 'Ravi', lastName: 'Teja' }
+
+// function fullName(greeting) {
+//     console.log(greeting, this.firstName, this.lastName)
+// }
+
+// fullName.call(obj, 'Hey')
+// fullName.apply(obj, ['Hello'])
+
+// ================================================
+
+// const studentObj = {
+//     name: 'Shyam',
+//     branch: 'CSE',
+//     studentDetails: function (age) {
+//         console.log("Name=", this.name, "Age=", age, "Branch=", this.branch)
+//     }
+// }
+
+// studentObj.studentDetails.call({ name: "Himanshu", branch: 'CSE' }, 21)
+// studentObj.studentDetails.apply({ name: "Himanshu", branch: 'ME' }, [23])
+
+// =====================
+// function Student(age) {
+//     console.log("Name=", this.name, "Age=", age, "Branch=", this.branch)
+// }
+
+// Student.call({ name: "Surya", branch:'CSE' }, 24)
+// Student.apply({ name: "Ravi", branch:'CSE' }, [25])
+
+// Bind Method
+// 1
+const person = { firstName: "Ravi", lastName: 'Raushan' }
+function greetFunction() {
+    console.log("Hello", this.firstName, this.lastName);
 }
-const evenNum = numArray.filter(checkEven);
-console.log(evenNum)
 
-// 3. Custom HOF
-function sayHello(index) {
-    console.log(index, "Hello World!")
+const greetPerson = greetFunction.bind(person)
+greetFunction(); // undefined
+greetPerson();
+
+// 2
+function multiply(a, b) {
+    console.log('a=',a,'b=', b)
+    return a * b;
 }
 
-function repeatFunction(func, n) {
-    for (let i = 0; i < n; i++) {
-        func(i)
+const double = multiply.bind(null, 2)
+console.log(double(5))
+console.log(double(10))
+// console.log(multiply(2, 5))
+
+// 3
+const studentObj = {
+    name: 'Shyam',
+    branch: 'CSE',
+    studentDetails: function (age) {
+        console.log("Name=", this.name, "Age=", age, "Branch=", this.branch)
     }
 }
 
-repeatFunction(sayHello, 3)
+const getStudentDetails = studentObj.studentDetails.bind(studentObj, 32)
+getStudentDetails()
 
-
-const numArray2 = [1, 2, 3, 4, 5];
-function callBackExample(numArray) {
-    let newArray = [];
-    for (let value of numArray) {
-        if (value % 2 === 0) {
-            newArray.push(value)
-        }
+// 4
+const personObj = {
+    name: 'Shyam',
+    getPerson: function(greeting){
+        console.log(greeting, this.name)
     }
-    return newArray
 }
-function getEvenNumber(fn) {
-    return fn(numArray2);
-}
-
-const abc = getEvenNumber(callBackExample)
-console.log(abc)
-
-// Function Accepting Callback Functions
-function add(a, b) {
-    console.log("Add function is called...")
-    return a + b;
-}
-
-function sub(a, b) {
-    console.log("Sub function is called...")
-    return a - b;
-}
-
-function Calculate(num1, num2, operation) {
-    return operation(num1, num2)
-}
-
-const result = Calculate(10, 5, sub)
-console.log(result)
+const person2 = personObj.getPerson.bind(personObj,'Hi');
+person2()
