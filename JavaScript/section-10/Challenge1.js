94378958049ea480fe5441e0426eb418d670b1a8
Ramdas Gupta <er.rdgupta@gmail.com>
2023-03-04 09:32:39 +0530

    Challenges

--- JavaScript/section-10/Challenge1.js
+++ JavaScript/section-10/Challenge1.js
@@ -36,4 +36,37 @@ const poll = {
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
   // This generates [0, 0, 0, 0]
   answers: new Array(4).fill(0),
+
+  registerNewAnswer() {
+    const answer = Number(prompt(this.question + "\n" + this.options.join("\n") + "\n(Write option number)"));
+    console.log(answer)
+
+    typeof answer === 'number' && answer < this.answers.length && this.answers[answer]++;
+    console.log("Answers", this.answers)
+
+    this.displayResults('array');
+    this.displayResults('string');
+  },
+
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers)
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`)
+    }
+  }
 };
+
+document.querySelector(".poll").addEventListener("click", poll.registerNewAnswer.bind(poll))
+
+poll.displayResults.call({answers : [5, 2, 3]}, 'array')
+poll.displayResults.call({answers : [5, 2, 3]}, 'string')
+poll.displayResults.call({answers : [1, 5, 3, 9, 6, 1]}, 'string')
+poll.displayResults.call({answers : [1, 5, 3, 9, 6, 1]})
+poll.displayResults.call({answers : [10,2,5,8]}, 'string')
+
+poll.displayResults.apply({answers : [5, 2, 3]}, ['array'])
+poll.displayResults.apply({answers : [5, 2, 3]}, ['string'])
+poll.displayResults.apply({answers : [1, 5, 3, 9, 6, 1]}, ['string'])
+poll.displayResults.apply({answers : [1, 5, 3, 9, 6, 1]})
+poll.displayResults.apply({answers : [10,2,5,8]}, ['string'])
