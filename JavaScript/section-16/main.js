45e1c40c9a1f2d1af898edade4d8a5a3ecdd5dcc
Ramdas Gupta <er.rdgupta@gmail.com>
2023-03-31 09:36:35 +0530

    Promises

--- JavaScript/section-16/main.js
+++ JavaScript/section-16/main.js
@@ -6,73 +6,154 @@
 // alert("HI")
 // console.log("This is text..")
 
-const pEl = document.querySelector("p")
-pEl.style.color = "red"
-console.log("Color changed")
-// alert("Hello")
-// pEl.textContent = "This is description text.."
+// const pEl = document.querySelector("p")
+// pEl.style.color = "red"
+// console.log("Color changed")
+// // alert("Hello")
+// // pEl.textContent = "This is description text.."
 
-// Asynchronous Example
-console.log("Hello World!")
-setTimeout(() => {
-    console.log("Inside Timeout")
-}, 5000)
-console.log("This is text..")
+// // Asynchronous Example
+// console.log("Hello World!")
+// setTimeout(() => {
+//     console.log("Inside Timeout")
+// }, 5000)
+// console.log("This is text..")
 
-setTimeout(() => {
-    pEl.textContent = "This is description text.."
-}, 5000)
-console.log("This is text..")
+// setTimeout(() => {
+//     pEl.textContent = "This is description text.."
+// }, 5000)
+// console.log("This is text..")
 
 
-// AJAX : Asynchronous Javascript AND XML
-// Allow us to communicate with the remote servers in Asynchronous way.
-// GET, POST ...etc
+// // AJAX : Asynchronous Javascript AND XML
+// // Allow us to communicate with the remote servers in Asynchronous way.
+// // GET, POST ...etc
 
-// API: Application Programming Interface
+// // API: Application Programming Interface
 
-// AJAX call
-const request = new XMLHttpRequest()
-request.open("GET", "https://jsonplaceholder.typicode.com/users")
-request.send()
+// // AJAX call
+// const request = new XMLHttpRequest()
+// request.open("GET", "https://jsonplaceholder.typicode.com/users")
+// request.send()
 
-request.addEventListener("load", function () {
-    const data = JSON.parse(this.responseText)
-    console.log("Data", data)
-    for(let user of data){
-        console.log(user.name)
-    }
-})
+// request.addEventListener("load", function () {
+//     const data = JSON.parse(this.responseText)
+//     console.log("Data", data)
+//     for(let user of data){
+//         console.log(user.name)
+//     }
+// })
 
-// Callback hell
+// // Callback hell
 
-function makeRequest1(callback){
-    // ... 
-    callback()
-}
+// function makeRequest1(callback){
+//     // ... 
+//     callback()
+// }
 
-function makeRequest2(callback){
-    // ... 
-    callback()
-}
+// function makeRequest2(callback){
+//     // ... 
+//     callback()
+// }
 
-function makeRequest3(callback){
-    // ... 
-    callback()
-}
+// function makeRequest3(callback){
+//     // ... 
+//     callback()
+// }
 
-function makeRequest4(callback){
-    // ... 
-    callback()
-}
+// function makeRequest4(callback){
+//     // ... 
+//     callback()
+// }
 
-makeRequest1(function(){
-    makeRequest2(function(){
-        makeRequest3(function(){
-            makeRequest4(function(){
-    
-            })
+// makeRequest1(function(){
+//     makeRequest2(function(){
+//         makeRequest3(function(){
+//             makeRequest4(function(){
+
+//             })
+//         })
+//     })
+// })
+
+// Promisis 
+
+// pending
+// fulfilled
+// rejected
+
+const requestUser = fetch("https://jsonplaceholder.typicode.com/users")
+console.log(requestUser)
+
+// fetch("https://jsonplaceholder.typicode.com/userss")
+//     .then(function (response) {
+//         console.log(response)
+//         return response.json()
+//     })
+//     .then(function (data) {
+//         console.log(data)
+//     })
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//     .then(function (response) {
+//         console.log(response)
+//         if (!response.ok)
+//             throw new Error("Something went wrong....")
+
+//         return response.json()
+//     })
+//     .then(function (data) {
+//         console.log(data)
+
+//     })
+
+const buttonEl = document.querySelector("button");
+const ulEl = document.querySelector("ul");
+
+buttonEl.addEventListener("click", function () {
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then(function (response) {
+            console.log(response)
+            if (!response.ok)
+                throw new Error("Something went wrong....")
+
+            return response.json()
         })
-    })
+        .then(function (data) {
+            for (let user of data) {
+                const liEl = document.createElement("li");
+                liEl.textContent = user.id + ". " + user.name
+                ulEl.appendChild(liEl)
+            }
+
+        })
 })
 
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(function (response) {
+        console.log(response)
+        if (!response.ok)
+            throw new Error("Error in calling all users API....")
+
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data)
+        return fetch("https://jsonplaceholder.typicode.com/users/" + data[0].id)
+    })
+    .then(function (response) {
+        console.log(response)
+        if (!response.ok)
+            throw new Error("Error in calling single user API....")
+
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data)
+    })
+    .catch(function (error) {
+        console.error(error)
+    })
+    .finally(function () {
+        console.log("Finally block calling...")
+    })
