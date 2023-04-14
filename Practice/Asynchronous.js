f1578010fc98210a79340161f9756d6768407190
Ramdas Gupta <er.rdgupta@gmail.com>
2023-04-14 09:16:11 +0530

    Practice

---
+++ Practice/Asynchronous.js
@@ -0,0 +1,8 @@
+// # Asynchronous JavaScript
+// 1. Callback hell
+// 2. Promises/ Consuming Promises/ Chaining Promises/ Handling Rejected Promises/ Throwing Errors Manually
+// 3. Fetch API
+// 4. Async/Await
+// 5. Error handling with try/catch
+// 6. Running Promises in Parallel
+// 7. Other Promise Combinators: race, allSettled and any
---
+++ Practice/Module.js
@@ -0,0 +1,17 @@
+// 1. Create a module that exports a function to generate random numbers between two given numbers.
+
+// 2. Create a module that exports a function to calculate the area of a circle, given its radius.
+
+// 3. Create a module that exports an object with properties for converting different units of measurement (e.g. length, weight, volume).
+
+// 4. Create a module that exports a class to represent a bank account, with methods for depositing, withdrawing, and checking the balance.
+
+// 5. Create a module that exports a function to generate a random password with a given length and complexity level (e.g. lowercase, uppercase, numbers, symbols).
+
+// 6. Create a module that exports a function to check if a given number is prime or not.
+
+// 7. Create a module that exports a function to calculate the factorial of a given number.
+
+// 8. Create a module that exports a function to sort an array of objects by a given property.
+
+// 9. Create a module that exports a function to convert a given string to title case (i.e. capitalize the first letter of each word).
